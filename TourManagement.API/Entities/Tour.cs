246f15fccdb73ace1599473958e6a896c90582ce
Sergii <sergiigrytsaienko@gmail.com>
2019-10-17 21:33:28 -0500

    init + migrations

---
+++ TourManagement.API/Entities/Tour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TourManagement.API.Entities
+{
+    public class Tour : AuditableEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TourId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public decimal EstimatedProfits { get; set; }
+
+        [Required]
+        public DateTimeOffset StartDate { get; set; }
+
+        [Required]
+        public DateTimeOffset EndDate { get; set; }
+
+        [Required]
+        public int BandId { get; set; }
+
+        [Required]
+        public Band Band { get; set; }
+
+        public int ManagerId { get; set; }
+
+        [ForeignKey("ManagerId")]
+        public Manager Manager { get; set; }
+
+        public ICollection<Show> Shows { get; set; } = new List<Show>();
+    }
+}
---
+++ TourManagement.API/Services/ITourManagementRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TourManagement.API.Entities;
+
+namespace TourManagement.API.Services
+{
+    public interface ITourManagementRepository
+    {
+        Task AddTour(Tour tour);
+        Task DeleteTour(Tour tour);
+        Task<Tour> GetTour(int tourId, bool includeShows = false);
+        Task<IEnumerable<Tour>> GetTours(bool includeShows = false);
+        Task<IEnumerable<Tour>> GetToursForManager(int managerId, bool includeShows = false);
+        Task<bool> IsTourManager(int tourId, int managerId);
+        Task<bool> SaveAsync();
+        Task<bool> TourExists(int tourId);
+        Task UpdateTour(Tour tour);
+        Task<IEnumerable<Show>> GetShows(int tourId);
+        Task<IEnumerable<Show>> GetShows(int tourId, IEnumerable<int> showIds);
+        Task AddShow(int tourId, Show show);
+        Task<IEnumerable<Band>> GetBands();
+        Task<IEnumerable<Manager>> GetManagers();
+    }
+}
